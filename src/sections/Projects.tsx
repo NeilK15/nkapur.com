6149720de866917c06e0252e9897852a03557d79
NeilK15 <neiltkapur@gmail.com>
2025-07-27 16:46:53 -0400

    added projects. added navbar breadcrumbs.

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -8,20 +8,43 @@ const Projects = () => {
                         <h3>Recipe Cookbook Full Stack App</h3>
                         <p>
                             A full-stack web application built to organize my
-                            family's recipes.
+                            family's recipes. The app is still in progress.
                         </p>
                         <div className="project-tech">
-                            <span>React</span>
+                            <span>Vite React</span>
                             <span>Node.js</span>
                             <span>MongoDB</span>
                         </div>
                         <div className="project-links">
-                            {/* <a href="#" className="btn btn-small">
-                                Live Demo
-                            </a> */}
                             <a
                                 href="https://github.com/NeilK15/kapur-home"
-                                className="btn btn-small btn-outline"
+                                target="_blank"
+                                className="btn btn-secondary btn-small btn-outline"
+                            >
+                                GitHub
+                            </a>
+                        </div>
+                    </div>
+                    <div className="project-card">
+                        <h3>Portfolio Website</h3>
+                        <p>
+                            A simple web application meant to showcase my
+                            education, work experience, and projects. I used
+                            React to build the front end and use AWS amplify to
+                            quickly and easily deploy.
+                        </p>
+                        <div className="project-tech">
+                            <span>Vite React</span>
+                            <span>AWS Amplify</span>
+                        </div>
+                        <div className="project-links">
+                            <a href="#" className="btn btn-primary btn-small">
+                                Live Demo
+                            </a>
+                            <a
+                                href="https://github.com/NeilK15/nkapur.com"
+                                target="_blank"
+                                className="btn btn-secondary btn-small btn-outline"
                             >
                                 GitHub
                             </a>
